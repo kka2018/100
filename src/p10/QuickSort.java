0b552a1259ed4e2e03b9a9d7fdfff9a16da5c17e
kishan <kishan.kushwaha11@gmail.com>
2018-08-18 13:39:42 +0530

    Quick sort

---
+++ src/p10/QuickSort.java
@@ -0,0 +1,50 @@
+package p10;
+
+import java.util.Scanner;
+
+public class QuickSort {
+
+    public static int partition(int[] arr, int start, int end){
+        int temp;
+        int pivot = arr[end];
+        int pIndex = start;
+        for(int i=start; i<end; i++){
+            if(arr[i]<=pivot){
+                temp = arr[i];
+                arr[i] = arr[pIndex];
+                arr[pIndex] = temp;
+                pIndex++;
+            }
+        }
+        temp = arr[end];
+        arr[end] = arr[pIndex];
+        arr[pIndex] = temp;
+        return pIndex;
+    }
+
+    public static void quicksort(int[] arr, int start, int end){
+        if(start<end) {
+            int pIndex = partition(arr, start, end);
+            quicksort(arr, start, pIndex - 1);
+            quicksort(arr, pIndex + 1, end);
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++)
+            a[i] = in.nextInt();
+
+        // Applying Quick sort
+
+        quicksort(a, 0, n-1);
+
+        for(int x : a)
+            System.out.print(x + " ");
+        in.close();
+    }
+}
